ad5dbbbce3236fbfccf0eae5c8b3deaa6ddb0d9e
amzxir <amirreza79ahmadi@gmail.com>
2023-12-25 12:31:44 +0330

    add livewire tree category and invoice create

---
+++ app/Livewire/Panel/Category/Tree.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panel\Category;
+
+use Livewire\Component;
+
+class Tree extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.category.tree');
+    }
+}
